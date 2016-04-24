66ed62b49315e7d12817739122c5beeaedaab7b4
kamekame <nanosecond@web.de>
2016-04-24 16:20:23 +0200

    first commit

---
+++ reaction_test.py
@@ -0,0 +1,22 @@
+
+"""
+This program shows you your reaction time
+"""
+
+import time
+import random
+n_items = 4
+duration = 0
+
+print("press Enter if you see the ########################### !")
+for i in range(n_items):
+    time.sleep(random.randint(3000, 10000)*1.0/1000)
+    print("#################################")
+    print("#################################")
+    print("#################################")
+    ts1 = time.time()
+    input()
+    ts2 = time.time()
+    duration += ts2-ts1
+
+print("Your reaction time is: " + str(duration/n_items) + " s")
