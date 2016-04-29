51c43deacfc1e8023074f6b2d7c08edbae246f4f
Steffen <nanosecond@web.de>
2016-04-29 22:28:01 +0200

    Felder?
    so, jetzt gibt es zwei Buttons mit Zahlen.
    Aber eigentlich bräuchte man Felder wo man etwas reinschreibt.

--- math_is_fun.py
+++ math_is_fun.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-# coding: utf-8
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
 
-# Author: Steffen Schneider und Erik Streb del Toro
-# Licence: GPL v3 or newer
-# Changelog:
-#   - 2016-04-28: first version
-#   - 2016-04-29: second version with tkinter
+# uuups deine Beschreibung ist weg. (Sorry)
 
 import tkinter
 from tkinter import Canvas
 
+"""
+TODO:
+- anstatt grid canvas. nutzen; damit lässt sich die Position pixelgenau einstellen
+"""
 
 class math_program(tkinter.Tk):
     def __init__(self, parent):
@@ -19,23 +19,26 @@ class math_program(tkinter.Tk):
 
     def initialize(self):
         self.grid()
-        self.geometry("400x400+300+150")
+        self.geometry("400x400+300+150")  # field size
+
         self.entryVariable = tkinter.StringVar()
         self.entry = tkinter.Entry(self, textvariable=self.entryVariable)
         self.entry.grid(column=0, row=0, sticky='EW')
-        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entry.bind("<Return>", self.OnPressEnter)  # keypress
         self.entryVariable.set(u"Enter text here.")
 
-        button = tkinter.Button(self, text=u"Click me !", command=self.OnButtonClick)
+        button = tkinter.Button(self, text=u"Click me !", command=self.OnButtonClick)  # button click
         button.grid(column=1, row=0)
 
-        canvas = Canvas(self)
-        canvas.create_rectangle(10, 10, 22, 22, outline="#79d", fill="#2d6")
+        button2 = tkinter.Button(self, text=u"3", command=self.OnButtonClick)
+        button2.grid(column=0, row=2)
+        button3 = tkinter.Button(self, text=u"4", command=self.OnButtonClick)
+        button3.grid(column=0, row=3)
 
+        # status line at the bottom
         self.labelVariable = tkinter.StringVar()
-        label = tkinter.Label(self, textvariable=self.labelVariable,
-                              anchor="w", fg="white", bg="blue")
-        label.grid(column=0, row=1, columnspan=2, sticky='EW')
+        label = tkinter.Label(self, textvariable=self.labelVariable, anchor="w", fg="white", bg="black")
+        label.grid(column=0, row=10, columnspan=2)#, sticky='EW')
         self.labelVariable.set(u"Hello !")
 
         self.grid_columnconfigure(0, weight=1)
