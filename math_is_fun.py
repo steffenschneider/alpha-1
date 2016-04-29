99df6c72a45fd0fdaee1b14cbc6b72bfc0a554f5
kamekame <nanosecond@web.de>
2016-04-29 21:44:38 +0200

    initial version

--- math_is_fun.py
+++ math_is_fun.py
@@ -1,18 +1,57 @@
-"""
-Math game for children of the age 5-7.
-"""
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
 
-# TODO
-# generate random numbers    5 min
-# get user input             5 min
-# scoring                    5 min
+import tkinter
+from tkinter import Canvas
 
-import matplotlib.pyplot as plt
 
-plt.plot([0, 1], [0, 0])
-font_size = 70
-plt.text(0.33, 0.02, r'1+1', fontsize=font_size)
-plt.text(0.3, -0.04, r'2', fontsize=font_size)
-plt.text(0.6, -0.04, r'3', fontsize=font_size)
+class math_program(tkinter.Tk):
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
 
-plt.show()
+    def initialize(self):
+        self.grid()
+        self.geometry("400x400+300+150")
+        self.entryVariable = tkinter.StringVar()
+        self.entry = tkinter.Entry(self, textvariable=self.entryVariable)
+        self.entry.grid(column=0, row=0, sticky='EW')
+        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter text here.")
+
+        button = tkinter.Button(self, text=u"Click me !", command=self.OnButtonClick)
+        button.grid(column=1, row=0)
+
+        canvas = Canvas(self)
+        canvas.create_rectangle(10, 10, 22, 22, outline="#79d", fill="#2d6")
+
+        self.labelVariable = tkinter.StringVar()
+        label = tkinter.Label(self, textvariable=self.labelVariable,
+                              anchor="w", fg="white", bg="blue")
+        label.grid(column=0, row=1, columnspan=2, sticky='EW')
+        self.labelVariable.set(u"Hello !")
+
+        self.grid_columnconfigure(0, weight=1)
+        self.resizable(True, False)
+        self.update()
+        self.geometry(self.geometry())
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def OnButtonClick(self):
+        self.labelVariable.set(self.entryVariable.get() + " (You clicked the button)")
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def OnPressEnter(self, event):
+        self.labelVariable.set(self.entryVariable.get() + " (You pressed ENTER)")
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+
+if __name__ == "__main__":
+    app = math_program(None)
+    app.title('Math is fun')
+
+    app.mainloop()
