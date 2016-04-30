7f021f8d4a4c324ecd4c882471d1901d6dde69a2
kamekame <nanosecond@web.de>
2016-04-30 20:33:51 +0200

    first working solution

--- math_is_fun.py
+++ math_is_fun.py
@@ -35,7 +35,7 @@ __author__ = "Steffen Schneider, Erik Streb"
 __copyright__ = "..."
 __credits__ = ["Steffen Schneider, Erik Streb"]
 __license__ = "GPL"
-__version__ = "0.1"
+__version__ = "0.5"
 __maintainer__ = "Steffen Schneider"
 __email__ = "nanosecond@web.de"
 __status__ = "Development"
@@ -51,12 +51,12 @@ class MathProgram(tkinter.Tk):
 
     def run_game(self):
         # create problem
-        int_1, int_2, solution, wrong_solution, solution_position = self.CreateProblem()
+        int_1, int_2, solution, wrong_solution, solution_position = self.create_problem()
 
         # text with the math question
         self.labelVariable = tkinter.StringVar()
         label = tkinter.Label(self, textvariable=self.labelVariable,
-                              anchor="center", fg="orange", bg="yellow", text="Helvetica",
+                              anchor="center", fg="black", bg="yellow", text="Helvetica",
                               font=("Helvetica", 45))
         label.grid(column=0, row=0, columnspan=2, sticky='EW')
         self.labelVariable.set(str(int_1) + " + " + str(int_2))
@@ -66,14 +66,16 @@ class MathProgram(tkinter.Tk):
         # answer fields (Buttons)
         answer_fields = [solution, wrong_solution]
         button2 = tkinter.Button(self, text=answer_fields[solution_position], font="Helvetica 65 bold",
-                                 command=self.OnButtonClickButton2, height=1, width=3, fg="black", bg="yellow")
+                                 command=lambda: self.check_solution(2, solution_position), height=1, width=3,
+                                 fg="black", bg="yellow")
         button2.place(relx=0.25, rely=0.5, anchor=tkinter.CENTER)
 
         button3 = tkinter.Button(self, text=answer_fields[1 ^ solution_position], font="Helvetica 65 bold",
-                                 command=self.OnButtonClickButton3, height=1, width=3, fg="black", bg="yellow")
+                                 command=lambda: self.check_solution(3, solution_position), height=1, width=3,
+                                 fg="black", bg="yellow")
         button3.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
 
-    def CreateProblem(self):
+    def create_problem(self):
         # create question
         int_1 = random.randint(0, 6)
         int_2 = random.randint(0, 6)
@@ -82,17 +84,12 @@ class MathProgram(tkinter.Tk):
         solution_position = random.randint(0, 1)  # change the position of the solution randomly
         return int_1, int_2, solution, wrong_solution, solution_position
 
-    def OnButtonClickButton2(self):
-        self.Check_solution(2)
+    def check_solution(self, button_number, solution_position):
+        if button_number - 2 == solution_position:
+            print("PASS")
+        else:
+            print("FAIL")
         self.run_game()
-
-    def OnButtonClickButton3(self):
-        self.Check_solution(3)
-        self.run_game()
-
-    def Check_solution(self, button_number):
-        # todo
-        pass
 
 
 if __name__ == "__main__":
