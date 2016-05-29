144191538173acc0de013887ba0ea68954705f9e
kamekame <nanosecond@web.de>
2016-05-29 16:33:03 +0200

    ...

---
+++ mytest.py
@@ -0,0 +1,15 @@
+import math
+import unittest
+
+
+class MyTests(unittest.TestCase):
+    def test_one(self):
+        print(111)
+
+    def test_2(self):
+        assert (1 != 2)
+        assert (1 == 1)
+        assert (math.sqrt(4) == 2)
+        assert isinstance("absdfk", str)
+        assert isinstance(123, int)
+        assert isinstance(1.2232, float)
