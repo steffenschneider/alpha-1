df028c0bedc413d92f8323664d7495c50966870b
kamekame <nanosecond@web.de>
2016-07-16 10:12:42 +0200

    ...

--- startup.py
+++ startup.py
@@ -2,34 +2,38 @@
 
 import random
 
-print("remove_pyc()")
+import lex_analyse_tags
+import lex_count_tags
+import lex_sort
+import linux
+import podcast_to_dropbox
 import remove_pyc
+
+print("remove_pyc()")
+
 
 remove_pyc.main()
 
 print("lex_analyse_tags()")
-import lex_analyse_tags
+
 
 lex_analyse_tags.main()
 
 print("lex_count_tags()")
-import lex_count_tags
 
 lex_count_tags.main()
 
-print("lex_sort()")
-import lex_sort
-
-lex_sort.main()
+rnd = random.randint(1, 8)
+if rnd == 1:
+    print("lex_sort()")
+    lex_sort.main()
 
 print("podcast_to_dropbox()")
-import podcast_to_dropbox
 
 podcast_to_dropbox.get_podcasts()
 
-rnd = random.randint(1, 10)
+rnd = random.randint(1, 20)
 if rnd == 1:
     print("update_system()")
-    import linux
 
     linux.update_system()
