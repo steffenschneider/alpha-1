71fa85f2f277aa7ffc44c22141fdd7261e5651db
kamekame <nanosecond@web.de>
2016-10-27 21:16:09 +0200

    ...

--- tools/import_listing.py
+++ tools/import_listing.py
@@ -1,6 +1,81 @@
+import datetime
+import glob
+import inspect
+import logging
+import math
 import os
+import random
+import re
+import socket
+import string
+import struct
+import subprocess
+import sys
+import time
+import urllib
+import webbrowser
+import xml.etree.ElementTree as ET
+from time import gmtime, strftime, localtime
+from tkinter import SUNKEN, Tk, Canvas, Frame
+
+import easygui
+import numpy
+import pillow
+import pyautoit
+import pymodbus3
+import pymouse
+import pymysql
+import pyvirtualdisplay
+import pywin32
+import pywinauto
+import requests
+import scipy
+import vpython
+from selenium import webdriver
+from visual import *
 
 #############################################################
 
 if os.name == 'nt':  # Windows
-    pass
+    import winsound
+
+# ignore!
+SUNKEN
+Tk
+Canvas
+Frame
+gmtime
+localtime
+strftime
+urllib
+time
+sys.argv
+vpython.arrow
+webbrowser.browser
+ET._Element_Py
+socket
+string
+struct
+re
+subprocess
+datetime
+easygui
+glob
+inspect
+logging
+math
+numpy
+os
+pillow
+pyautoit
+pymodbus3
+pymouse
+pymysql
+pyvirtualdisplay
+pywin32
+pywinauto
+random
+requests
+scipy
+webdriver
+winsound
