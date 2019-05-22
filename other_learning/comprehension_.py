4867641b1239d5037f3aa4588cabf418eeb8adaf
steffenschneider <33177732+steffenschneider@users.noreply.github.com>
2019-05-22 16:10:19 +0200

    Delete comprehension_.py

--- other_learning/comprehension_.py
+++
@@ -1,7 +0,0 @@
-## sum all digits
-number = "-12345"
-sum(int(d) for d in str(abs(number)))
-
-## capitalize all words in a line
-string = "This is my sentence"
-' '.join([word.capitalize() for word in string.split()])
